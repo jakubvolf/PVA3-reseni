007d352c90bdccf234c57f73c2a8492dd37463ea
jakubvolf <57982104+jakubvolf@users.noreply.github.com>
2019-12-04 08:20:17 +0100

    Create task03.cpp

---
+++ task03.cpp
@@ -0,0 +1,15 @@
+/*
+Decode the task02.dat file and find out the birth dates of John Doe and Sam Sepiol.
+format: N records of 6 bytes:
+  byte 1-2: birth date yyyyyyyd ddddmmmm (year of birth LSB - 1900)
+  byte 3-6: height (float LSB)
+separator 0xFF (after N records)
+name1|name2|...|nameN|
+*/
+
+
+#include <cstdio>
+
+int main() {
+  // TODO
+}
