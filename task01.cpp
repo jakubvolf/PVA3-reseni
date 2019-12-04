9afc426d1f4d313fcb389f530805f7595b1e83bf
jakubvolf <57982104+jakubvolf@users.noreply.github.com>
2019-12-04 08:08:04 +0100

    Update task01.cpp

--- task01.cpp
+++ task01.cpp
@@ -1,12 +1,25 @@
 #include <cstdio>
+#include <string.h>
+char result[64];
 
-char* toUpper(char* input) {
-	int i;
-	int delka;
-	delka strlen (input);
+
+const char* toUpper(const char* input) {
+	  int counter = 0;
+	  while (*input != '\0') {
+	   if(*input > 96 && *input < 123){ // Find if letter is uppercase or not
+		    result[counter] = *input - 32; // transfer to uppercase
+		}
+		else{
+      result[counter] = *input; // letter is already uppercase
+    }
+		counter++;
+		input++;
+	}
+	return result;
 }
 
 int main() {
-	char input[] = "Hello, World!";
+	const char* input = "Hello, World!";
 	puts(toUpper(input)); // should print HELLO, WORLD!
+
 }
