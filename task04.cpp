5a22a8e25b788c39a68496dcc36b47c1a7d939f7
jakubvolf <57982104+jakubvolf@users.noreply.github.com>
2020-03-04 09:34:56 +0100

    Update task04.cpp

--- task04.cpp
+++ task04.cpp
@@ -1,25 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
 struct Animal {
+
   const char* name;
-  virtual const char* whatDoesItSay();
-  // TODO
-}
+  const char* say;
+  char pole[255];
+  
+  virtual const char* whatDoesItSay(){
+    strcpy(pole, name);
+    strcat(pole, " dela ");
+    strcat(pole, say);
+    puts(pole);
+    return 0;
+};
+
+    Animal(const char* name, const char* say){
+    this->name = name;
+    this->say = say;
+    }
+};
 
 struct Dog : Animal {
-  // TODO
-}
+     Dog(const char* name, const char* say) : Animal(name, say){}
+};
 
 struct Fox : Animal {
-  // TODO
-}
+    Fox(const char* name, const char* say) : Animal(name, say){}
+};
 
 int main() {
+
   Animal* animals[] = {
     new Dog("dog", "woof"),
     new Fox("fox", "ringidingidingidingiding")
-  }
+  };
   for(Animal* animal : animals) {
     animal->whatDoesItSay();
   }
-  // dog says woof
-  // fox says ringidingidingidingiding
 }
